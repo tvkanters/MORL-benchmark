e22035f655aeeeacb4887a1e1b7b8d9cfbda54e1
tvkanters <tvkanters@gmail.com>
2014-09-26 16:15:39 +0200

    Removed WAIT action for finite horizon problems

--- Benchmark Resource Gathering/src/nl/uva/morlb/rg/environment/ResourceGatheringEnv.java
+++ Benchmark Resource Gathering/src/nl/uva/morlb/rg/environment/ResourceGatheringEnv.java
@@ -97,7 +97,7 @@ public class ResourceGatheringEnv implements EnvironmentInterface {
             taskSpec.addContinuousAction(new DoubleRange(-mParameters.maxStepSize, mParameters.maxStepSize));
             taskSpec.addContinuousAction(new DoubleRange(-mParameters.maxStepSize, mParameters.maxStepSize));
         } else {
-            taskSpec.addDiscreteAction(new IntRange(0, mParameters.actionMax));
+            taskSpec.addDiscreteAction(new IntRange((mParameters.finiteHorizon ? 1 : 0), mParameters.actionMax));
         }
 
         // Set the (in)finite horizon property
