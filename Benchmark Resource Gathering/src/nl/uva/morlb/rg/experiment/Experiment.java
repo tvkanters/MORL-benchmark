4475ac0dab92e1914bff8954249dbd5878ce98f3
tvkanters <tvkanters@gmail.com>
2014-10-08 16:23:50 +0200

    Fixed issue with random seed

--- Benchmark Resource Gathering/src/nl/uva/morlb/rg/experiment/Experiment.java
+++ Benchmark Resource Gathering/src/nl/uva/morlb/rg/experiment/Experiment.java
@@ -29,7 +29,6 @@ public class Experiment {
      * Runs the full experiment.
      */
     public void runExperiment() {
-        final Scalarisation scalarisation = new LinearScalarisation();
 
         for (int test = 1; test <= 10; ++test) {
             Log.f("\n\n========== TEST " + test + " ==========\n\n");
@@ -48,6 +47,9 @@ public class Experiment {
                     String metrics = "";
 
                     final SolutionSet solutionSet = new SolutionSet(solutionSetString);
+
+                    // Scalarisation must be created here for random seet purposes
+                    final Scalarisation scalarisation = new LinearScalarisation();
 
                     // Calculate non-reference metrics
                     final double[] avgRew = Judge.averageReward(solutionSet, scalarisation);
